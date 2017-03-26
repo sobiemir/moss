dc2caa98667db70f751896c8ed5598b946d45381
sobiemir <sobiemir@aculo.pl>
2017-03-26 13:13:14 +0200

    Usuwanie UNROLL dla haszy

--- moss/src/hash.c
+++ moss/src/hash.c
@@ -64,30 +64,9 @@ uint32_t ms_hash_32_djb2( const void *data, size_t length )
     uint32_t    hash = 5381;
     const char *cdat = data;
 
-    // UNROLL dla DJB2
-#define __MSU_HASH_32_DJB2 \
-    hash = ((hash << 5) + hash) + *cdat++
-
-    // pojedynczy przebieg
-#if !(defined MSD_HASH_LOOP_UNROLL) || MSD_HASH_LOOP_UNROLL < 2
-
     for( ; length > 0; --length )
         hash = ((hash << 5) + hash) + *cdat++;
 
-    // kilkukrotny przebieg
-#else
-
-    for( ; length > (MSD_HASH_LOOP_UNROLL - 1); length -= MSD_HASH_LOOP_UNROLL )
-        MSX_LOOP_UNROLL_ONELINE_MACRONUM( __MSU_HASH_32_DJB2, MSD_HASH_LOOP_UNROLL );
-    
-    // licz pozostałości po pętli
-    switch( length )
-    {
-        MSX_CASE_UNROLL_NOBREAK_MACRONUM( __MSU_HASH_32_DJB2, MSD_HASH_LOOP_UNROLL );
-        break;
-    }
-
-#endif
     return hash;
 }
 
@@ -102,30 +81,9 @@ uint32_t ms_hash_32_djb2a( const void *data, size_t length )
     uint32_t    hash = 5381;
     const char *cdat = data;
 
-    // UNROLL dla DJB2
-#define __MSU_HASH_32_DJB2A \
-    hash = ((hash << 5) + hash) ^ (uint32_t)*cdat++
-
-    // pojedynczy przebieg
-#if !(defined MSD_HASH_LOOP_UNROLL) || MSD_HASH_LOOP_UNROLL < 2
-
     for( ; length > 0; --length )
         hash = ((hash << 5) + hash) ^ (uint32_t)*cdat++;
 
-    // kilkukrotny przebieg
-#else
-
-    for( ; length > (MSD_HASH_LOOP_UNROLL - 1); length -= MSD_HASH_LOOP_UNROLL )
-        MSX_LOOP_UNROLL_ONELINE_MACRONUM( __MSU_HASH_32_DJB2A, MSD_HASH_LOOP_UNROLL );
-    
-    // licz pozostałości po pętli
-    switch( length )
-    {
-        MSX_CASE_UNROLL_NOBREAK_MACRONUM( __MSU_HASH_32_DJB2A, MSD_HASH_LOOP_UNROLL );
-        break;
-    }
-
-#endif
     return hash;
 }
 
@@ -140,30 +98,9 @@ uint32_t ms_hash_32_sdbm( const void *data, size_t length )
 	uint32_t    hash = 0;
     const char *cdat = data;
 
-    // UNROLL dla SDBM
-#define __MSU_HASH_32_SDBM \
-    hash = *cdat++ + (hash << 6) + (hash << 16) - hash
-
-    // pojedynczy przebieg
-#if !(defined MSD_HASH_LOOP_UNROLL) || MSD_HASH_LOOP_UNROLL < 2
-
     for( ; length > 0; --length )
         hash = *cdat++ + (hash << 6) + (hash << 16) - hash;
 
-    // kilkukrotny przebieg
-#else
-
-    for( ; length > (MSD_HASH_LOOP_UNROLL - 1); length -= MSD_HASH_LOOP_UNROLL )
-        MSX_LOOP_UNROLL_ONELINE_MACRONUM( __MSU_HASH_32_SDBM, MSD_HASH_LOOP_UNROLL );
-    
-    // licz pozostałości po pętli
-    switch( length )
-    {
-        MSX_CASE_UNROLL_NOBREAK_MACRONUM( __MSU_HASH_32_SDBM, MSD_HASH_LOOP_UNROLL );
-        break;
-    }
-
-#endif
 	return hash;
 }
 
@@ -178,30 +115,9 @@ uint32_t ms_hash_32_joaat( const void *data, size_t length )
 	uint32_t    hash = 0;
     const char *cdat = data;
 
-    // UNROLL dla JOAAT
-#define __MSU_HASH_32_JOAAT \
-    (hash += *cdat++, hash += hash << 10, hash ^= hash >> 6)
-
-    // pojedynczy przebieg
-#if !(defined MSD_HASH_LOOP_UNROLL) || MSD_HASH_LOOP_UNROLL < 2
-
     for( ; length > 0; --length )
         hash += *cdat++, hash += hash << 10, hash ^= hash >> 6;
 
-    // kilkukrotny przebieg
-#else
-
-    for( ; length > (MSD_HASH_LOOP_UNROLL - 1); length -= MSD_HASH_LOOP_UNROLL )
-        MSX_LOOP_UNROLL_ONELINE_MACRONUM( __MSU_HASH_32_JOAAT, MSD_HASH_LOOP_UNROLL );
-    
-    // licz pozostałości po pętli
-    switch( length )
-    {
-        MSX_CASE_UNROLL_NOBREAK_MACRONUM( __MSU_HASH_32_JOAAT, MSD_HASH_LOOP_UNROLL );
-        break;
-    }
-
-#endif
 	hash += hash << 3;
 	hash ^= hash >> 11;
 	hash += hash << 15;
@@ -220,32 +136,11 @@ uint32_t ms_hash_32_fnv1( const void *data, size_t length )
     uint32_t    hash = 2166136261u;
     const char *cdat = data;
 
-    // UNROLL dla FNV1
-#define __MSU_HASH_32_FNV1 \
-    (hash += (hash << 1u) + (hash << 4u) + (hash << 7u) + (hash << 8u) + (hash << 24u), \
-     hash ^= *cdat++)
-
-    // pojedynczy przebieg
-#if !(defined MSD_HASH_LOOP_UNROLL) || MSD_HASH_LOOP_UNROLL < 2
-
     for( ; length > 0; --length )
-        hash += (hash << 1u) + (hash << 4u) + (hash << 7u) + (hash << 8u) + (hash << 24u),
+        hash += (hash << 1u) + (hash << 4u) + (hash << 7u) +
+                (hash << 8u) + (hash << 24u),
         hash ^= *cdat++;
 
-    // kilkukrotny przebieg
-#else
-
-    for( ; length > (MSD_HASH_LOOP_UNROLL - 1); length -= MSD_HASH_LOOP_UNROLL )
-        MSX_LOOP_UNROLL_ONELINE_MACRONUM( __MSU_HASH_32_FNV1, MSD_HASH_LOOP_UNROLL );
-    
-    // licz pozostałości po pętli
-    switch( length )
-    {
-        MSX_CASE_UNROLL_NOBREAK_MACRONUM( __MSU_HASH_32_FNV1, MSD_HASH_LOOP_UNROLL );
-        break;
-    }
-
-#endif
     return hash;
 }
 
@@ -260,32 +155,11 @@ uint64_t ms_hash_64_fnv1( const void *data, size_t length )
     uint64_t    hash = 14695981039346656037ull;
     const char *cdat = data;
 
-    // UNROLL dla FNV1
-#define __MSU_HASH_64_FNV1 \
-    (hash += (hash << 1ull) + (hash << 4ull) + (hash << 5ull) + (hash << 7ull) + (hash << 8ull) + (hash << 40ull), \
-     hash ^= *cdat++)
-
-    // pojedynczy przebieg
-#if !(defined MSD_HASH_LOOP_UNROLL) || MSD_HASH_LOOP_UNROLL < 2
-
     for( ; length > 0; --length )
-        hash += (hash << 1ull) + (hash << 4ull) + (hash << 5ull) + (hash << 7ull) + (hash << 8ull) + (hash << 40ull),
+        hash += (hash << 1ull) + (hash << 4ull) + (hash << 5ull) +
+                (hash << 7ull) + (hash << 8ull) + (hash << 40ull),
         hash ^= *cdat++;
 
-    // kilkukrotny przebieg
-#else
-
-    for( ; length > (MSD_HASH_LOOP_UNROLL - 1); length -= MSD_HASH_LOOP_UNROLL )
-        MSX_LOOP_UNROLL_ONELINE_MACRONUM( __MSU_HASH_64_FNV1, MSD_HASH_LOOP_UNROLL );
-    
-    // licz pozostałości po pętli
-    switch( length )
-    {
-        MSX_CASE_UNROLL_NOBREAK_MACRONUM( __MSU_HASH_64_FNV1, MSD_HASH_LOOP_UNROLL );
-        break;
-    }
-
-#endif
     return hash;
 }
 
@@ -300,32 +174,11 @@ uint32_t ms_hash_32_fnv1a( const void *data, size_t length )
     uint32_t    hash = 2166136261u;
     const char *cdat = data;
 
-    // UNROLL dla FNV1
-#define __MSU_HASH_32_FNV1A \
-    (hash ^= *cdat++, \
-     hash += (hash << 1u) + (hash << 4u) + (hash << 7u) + (hash << 8u) + (hash << 24u))
-
-    // pojedynczy przebieg
-#if !(defined MSD_HASH_LOOP_UNROLL) || MSD_HASH_LOOP_UNROLL < 2
-
     for( ; length > 0; --length )
         hash ^= *cdat++,
-        hash += (hash << 1u) + (hash << 4u) + (hash << 7u) + (hash << 8u) + (hash << 24u);
+        hash += (hash << 1u) + (hash << 4u) + (hash << 7u) +
+                (hash << 8u) + (hash << 24u);
 
-    // kilkukrotny przebieg
-#else
-
-    for( ; length > (MSD_HASH_LOOP_UNROLL - 1); length -= MSD_HASH_LOOP_UNROLL )
-        MSX_LOOP_UNROLL_ONELINE_MACRONUM( __MSU_HASH_32_FNV1A, MSD_HASH_LOOP_UNROLL );
-    
-    // licz pozostałości po pętli
-    switch( length )
-    {
-        MSX_CASE_UNROLL_NOBREAK_MACRONUM( __MSU_HASH_32_FNV1A, MSD_HASH_LOOP_UNROLL );
-        break;
-    }
-
-#endif
     return hash;
 }
 
@@ -340,32 +193,11 @@ uint64_t ms_hash_64_fnv1a( const void *data, size_t length )
     uint64_t    hash = 14695981039346656037ull;
     const char *cdat = data;
 
-    // UNROLL dla FNV1
-#define __MSU_HASH_64_FNV1A \
-    (hash ^= *cdat++, \
-     hash += (hash << 1ull) + (hash << 4ull) + (hash << 5ull) + (hash << 7ull) + (hash << 8ull) + (hash << 40ull))
-
-    // pojedynczy przebieg
-#if !(defined MSD_HASH_LOOP_UNROLL) || MSD_HASH_LOOP_UNROLL < 2
-
     for( ; length > 0; --length )
         hash ^= *cdat++,
-        hash += (hash << 1ull) + (hash << 4ull) + (hash << 5ull) + (hash << 7ull) + (hash << 8ull) + (hash << 40ull);
+        hash += (hash << 1ull) + (hash << 4ull) + (hash << 5ull) +
+                (hash << 7ull) + (hash << 8ull) + (hash << 40ull);
 
-    // kilkukrotny przebieg
-#else
-
-    for( ; length > (MSD_HASH_LOOP_UNROLL - 1); length -= MSD_HASH_LOOP_UNROLL )
-        MSX_LOOP_UNROLL_ONELINE_MACRONUM( __MSU_HASH_64_FNV1A, MSD_HASH_LOOP_UNROLL );
-    
-    // licz pozostałości po pętli
-    switch( length )
-    {
-        MSX_CASE_UNROLL_NOBREAK_MACRONUM( __MSU_HASH_64_FNV1A, MSD_HASH_LOOP_UNROLL );
-        break;
-    }
-
-#endif
     return hash;
 }
 
@@ -380,46 +212,21 @@ uint32_t ms_hash_32_murmur3( const void *data, size_t length )
     uint32_t hash = MSD_HASH_MURMUR_SEED,
              ch4b;
     size_t   iter;
+
     const uint32_t *cdat = data;
     const char     *adat;
 
-    // rozwinięcie pętli dla MURMUR3
-#define __MSU_HASH_32_MURMUR3 \
-    (ch4b  = *cdat++ * 0xcc9e2d51, \
-     hash ^= (ROTL32(ch4b, 15)) * 0x1b873593, \
-     hash  = ROTL32(hash, 13), \
-     hash += (hash << 2) + 0xe6546b64)
-
-    // brak rozwinięcia, lub rozwinięcie mniejsze lub równe 4
-    // murmur i tak już pobiera porcjami 4 bajtowymi
-#if !(defined MSD_HASH_LOOP_UNROLL) || (MSD_HASH_LOOP_UNROLL >> 2) < 2
-
+    // murmur3 pobiera porcjami 4 bajtowymi
     for( iter = length >> 2; iter > 0; --iter )
         ch4b  = *cdat++ * 0xcc9e2d51,
         hash ^= (ROTL32(ch4b, 15)) * 0x1b873593,
         hash  = ROTL32(hash, 13),
         hash += (hash << 2) + 0xe6546b64;
 
-    // a tu jeżeli rozwinięcie jest koniecznie wymagane
-#else
-
-    size_t len = MSD_HASH_LOOP_UNROLL >> 2;
-    for( iter = length >> 2; iter > (len - 1); iter -= len )
-        MSX_LOOP_UNROLL_ONELINE_MACRONUM( __MSU_HASH_32_MURMUR3, MSD_HASH_LOOP_UNROLL );
-
-    // licz pozostałości po pętli
-    switch( iter )
-    {
-        MSX_CASE_UNROLL_NOBREAK_MACRONUM( __MSU_HASH_32_MURMUR3, MSD_HASH_LOOP_UNROLL );
-        break;
-    }
-
-#endif
-
     adat = (const char*)cdat;
     ch4b = 0;
 
-    // murmur pobiera 4 bajty, więc jakoś musi skonsumować mniejszą ilość
+    // konsumpcja pozostałych wartości
     switch( length & 3 )
     {
         case 3: ch4b |= adat[2] << 16;
